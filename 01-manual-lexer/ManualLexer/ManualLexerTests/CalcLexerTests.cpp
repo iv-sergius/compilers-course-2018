29cbadb954cfb0e13ad04cfd2c093dca25418a3c
Kerim Arnaberdiyev <kerim.arnaberdiyev@gmail.com>
2018-09-05 00:03:27 +0300

    Добавлены тесты

--- 01-manual-lexer/ManualLexer/ManualLexerTests/CalcLexerTests.cpp
+++ 01-manual-lexer/ManualLexer/ManualLexerTests/CalcLexerTests.cpp
@@ -84,11 +84,24 @@ TEST_CASE("Can read expression tokens", "[CalcLexer]") {
 		Token{ TT_PLUS },
 		Token{ TT_NUMBER, "28"s },
 		});
+#if 0 // fractional number support
+	REQUIRE(Tokenize("5+7.005"sv) == TokenList{
+		Token{ TT_NUMBER, "5" },
+		Token{ TT_PLUS },
+		Token{ TT_NUMBER, "7.005" },
+		});
+	REQUIRE(Tokenize("1.005+43.54+1"sv) == TokenList{
+		Token{ TT_NUMBER, "1.005" },
+		Token{ TT_PLUS },
+		Token{ TT_NUMBER, "43.54" },
+		Token{ TT_PLUS },
+		Token{ TT_NUMBER, "1" },
+		});
+#endif
 }
 
 #if 0 // whitespace support
-// TODO: this test should pass OK.
-TEST_CASE("Can read one operator after whitespace", "[CalcLexer]") {
+TEST_CASE("Can read one operator with whitespaces", "[CalcLexer]") {
 	REQUIRE(Tokenize("  +"sv) == TokenList{
 		Token{ TT_PLUS },
 		});
@@ -98,14 +111,128 @@ TEST_CASE("Can read one operator after whitespace", "[CalcLexer]") {
 	REQUIRE(Tokenize("   \t\t+"sv) == TokenList{
 		Token{ TT_PLUS },
 		});
+	REQUIRE(Tokenize("\n+"sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("   \n  +"sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("\t   \n  +"sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("+    "sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("+  \t\t   "sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("+  \n\t   "sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("   +   "sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("  \t +  \t "sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("  \t\t +  \t "sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+	REQUIRE(Tokenize("  \t \t +  \n\t "sv) == TokenList{
+		Token{ TT_PLUS },
+		});
+}
+
+TEST_CASE("Can read one number with whitespaces", "[CalcLexer]") {
+	REQUIRE(Tokenize("  1"sv) == TokenList{
+		Token{ TT_NUMBER, "1" },
+		});
+	REQUIRE(Tokenize("\t4"sv) == TokenList{
+		Token{ TT_NUMBER, "4" },
+		});
+	REQUIRE(Tokenize("   \t\t3.2"sv) == TokenList{
+		Token{ TT_NUMBER, "3.2" },
+		});
+	REQUIRE(Tokenize("\n9"sv) == TokenList{
+		Token{ TT_NUMBER, "9" },
+		});
+	REQUIRE(Tokenize("   \n  15"sv) == TokenList{
+		Token{ TT_NUMBER, 15 },
+		});
+	REQUIRE(Tokenize("\t   \n  21.03"sv) == TokenList{
+		Token{ TT_NUMBER, "21.03" },
+		});
+	REQUIRE(Tokenize("0    "sv) == TokenList{
+		Token{ TT_NUMBER, "0" },
+		});
+	REQUIRE(Tokenize("81  \t\t   "sv) == TokenList{
+		Token{ TT_NUMBER, "81" },
+		});
+	REQUIRE(Tokenize("4.2  \n\t   "sv) == TokenList{
+		Token{ TT_NUMBER, "4.2" },
+		});
+	REQUIRE(Tokenize("   7.9   "sv) == TokenList{
+		Token{ TT_NUMBER, "7.9" },
+		});
+	REQUIRE(Tokenize("  \t 3  \t "sv) == TokenList{
+		Token{ TT_NUMBER, "3" },
+		});
+	REQUIRE(Tokenize("  \t\t 9.001  \t "sv) == TokenList{
+		Token{ TT_NUMBER, "9.001" },
+		});
+	REQUIRE(Tokenize("  \t \t 7  \n\t "sv) == TokenList{
+		Token{ TT_NUMBER, "7" },
+		});
+}
+
+TEST_CASE("Can read expression tokens with whitespaces") {
+	REQUIRE(Tokenize("2 + 3"sv) == TokenList{
+		Token{ TT_NUMBER, "2" },
+		Token{ TT_PLUS },
+		Token{ TT_NUMBER, "3" },
+		});
+	REQUIRE(Tokenize("\t0.52 + \n4"sv) == TokenList{
+		Token{ TT_NUMBER, "0.52" },
+		Token{ TT_PLUS },
+		Token{ TT_NUMBER, "4" },
+		});
+	REQUIRE(Tokenize("\n+ \t7.1"sv) == TokenList{
+		Token{ TT_PLUS },
+		Token{ TT_NUMBER, "7.1" },
+		});
 }
 #endif
 
-#if 0 // extended numbers support
-// TODO: this test should pass OK.
+#if 0 
 TEST_CASE("Cannot read number which starts with zero") {
 	REQUIRE(Tokenize("0123456789"sv) == TokenList{
 		Token{ TT_ERROR },
+		});
+	REQUIRE(Tokenize("01.25"sv) == TokenList{
+		Token{ TT_ERROR },
+		});
+	REQUIRE(Tokenize("+01"sv) == TokenList{
+		Token{ TT_PLUS },
+		Token{ TT_ERROR },
+		});
+	REQUIRE(Tokenize("+00.32"sv) == TokenList{
+		Token{ TT_PLUS },
+		Token{ TT_ERROR },
+		});
+	REQUIRE(Tokenize("4+0521"sv) == TokenList{
+		Token{ TT_NUMBER, "4" },
+		Token{ TT_PLUS },
+		Token{ TT_ERROR },
+		});
+	REQUIRE(Tokenize("02+21"sv) == TokenList{
+		Token{ TT_ERROR },
+		Token{ TT_PLUS },
+		Token{ TT_NUMBER, "21" },
+		});
+	REQUIRE(Tokenize("02.4+5.3"sv) == TokenList{
+		Token{ TT_ERROR },
+		Token{ TT_PLUS },
+		Token{ TT_NUMBER, "5.3" },
 		});
 }
 #endif
