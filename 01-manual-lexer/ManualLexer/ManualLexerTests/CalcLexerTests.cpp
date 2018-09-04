db7536a05b4727cd4810c328f0618dfe62ac1e1f
Sergey Shambir <sergey.shambir@ispringsolutions.com>
2018-09-04 21:09:22 +0300

    Added 01-manual-lexer skeleton

---
+++ 01-manual-lexer/ManualLexer/ManualLexerTests/CalcLexerTests.cpp
@@ -0,0 +1,86 @@
+#include "catch2/catch.hpp"
+#include "../ManualLexer/CalcLexer.h"
+#include <vector>
+
+using namespace std;
+using namespace calc;
+
+namespace calc
+{
+bool operator ==(const Token& a, const Token& b)
+{
+	return a.type == b.type && a.value == b.value;
+}
+
+string_view GetTokenName(TokenType type)
+{
+	switch (type)
+	{
+	case calc::TT_END:
+		return "end";
+	case calc::TT_ERROR:
+		return "error";
+	case calc::TT_NUMBER:
+		return "number";
+	case calc::TT_PLUS:
+		return "+";
+	}
+	return "<UNEXPECTED!!!>";
+}
+
+std::ostream& operator<<(std::ostream& stream, const Token& token)
+{
+	stream << "Token(" << GetTokenName(token.type);
+	if (token.value)
+	{
+		stream << ", " << *token.value;
+	}
+	stream << ")";
+	return stream;
+}
+}
+
+namespace
+{
+
+using TokenList = vector<Token>;
+
+TokenList Tokenize(string_view text)
+{
+	TokenList results;
+	CalcLexer lexer{ text };
+	for (Token token = lexer.Read(); token.type != TT_END; token = lexer.Read())
+	{
+		results.emplace_back(move(token));
+	}
+	return results;
+}
+
+}
+
+TEST_CASE("Can parse one number", "[CalcLexer]") {
+	REQUIRE(Tokenize("0"sv) == TokenList{
+		Token{ TT_NUMBER, "0"s },
+		});
+	REQUIRE(Tokenize("1"sv) == TokenList{
+		Token{ TT_NUMBER, "1"s },
+		});
+	REQUIRE(Tokenize("9876543210"sv) == TokenList{
+		Token{ TT_NUMBER, "9876543210"s },
+		});
+}
+
+TEST_CASE("Can parse one operator", "[CalcLexer]") {
+	REQUIRE(Tokenize("9876543210"sv) == TokenList{
+		Token{ TT_NUMBER, "9876543210"s },
+		});
+}
+
+#if 0
+// TODO: this test should pass OK.
+TEST_CASE("Cannot read number which starts with zero") {
+	REQUIRE(Tokenize("0123456789"sv) == TokenList{
+		Token{ TT_ERROR },
+		});
+}
+#endif
